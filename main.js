688b5ee8deb7b3f548d7b81ebe3d2fe62dfe37ae
LostoVayne <moises.barrera.dev@gmail.com>
2023-06-23 16:47:08 -0400

    fixed

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+// Importaciones
 const express = require('express')
-const app = express()
-const port = 3000
-require('dotenv').config()
 const Fetch_Data = require('./helpers/Fetching_Data.js')
 const transporter = require('./helpers/mail')
+const path = require('path')
+const fs = require('fs')
+const { v4: uuidv4 } = require('uuid')
+require('dotenv').config()
+const app = express()
+const port = 3000
 
 app.use(express.static('public'))
 
@@ -14,15 +18,36 @@ app.get('/', (req, res) => {
 app.get('/correos', async (req, res) => {
   const { para, asunto, contenido } = req.query
   const { dolar, euro, uf, utm } = await Fetch_Data()
+
+  // Creacion del Template
+
   const DataEmail = {
     from: `Epsaind Developer ${process.env.EMAIL}`,
     to: para,
     subject: asunto,
-    text: `${contenido}: El valor del ${dolar.codigo} es: ${dolar.valor} , El valor del ${euro.codigo} es: ${euro.valor} , El valor del ${uf.codigo} es: ${uf.valor} , El valor del ${utm.codigo} es: ${utm.valor} `
+    text: `${contenido}: 
+    El valor del ${dolar.codigo} es: ${dolar.valor} , 
+    El valor del ${euro.codigo} es: ${euro.valor} , 
+    El valor del ${uf.codigo} es: ${uf.valor} , 
+    El valor del ${utm.codigo} es: ${utm.valor} `
   }
 
+  //Parseo de los datos
+
   const result = await transporter.sendMail(DataEmail)
+
+  const EmailParseado = JSON.stringify(DataEmail)
+
+  const filePath = path.join(__dirname, 'correos', `${uuidv4().slice(0, 8)}.txt`)
   console.log(result)
+
+  // Manejo del File System
+
+  fs.writeFile(filePath, EmailParseado, (err) => {
+    if (err) throw err
+    console.log('The file has been saved!')
+  })
+
   res.status(200).json({ ok: 'true', message: 'Correo electrónico enviado exitosamente' })
 })
 
